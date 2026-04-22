4638c0b7614d770aa7e8a4322fac9ee6412e1d74
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-04-22 12:36:22 +0545

    add authmiddleware in between

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -9,8 +9,8 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.get('/me',authMiddleware, getMeController);
-router.put('/me', updateMeController);
-router.put('/me/password', changePasswordController);
+router.get('/me', authMiddleware, getMeController);
+router.put('/me', authMiddleware, updateMeController);
+router.put('/me/password', authMiddleware, changePasswordController);
 
 export default router;
